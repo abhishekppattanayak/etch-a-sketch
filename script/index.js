4d500676c572624eb7e3d4ecc06ef99c4a0fcd83
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2023-12-24 21:25:55 +0530

    Fix draw and add eraser

--- script/index.js
+++ script/index.js
@@ -1,22 +1,47 @@
 document.querySelector('footer').textContent = `Copyright © ${new Date().getFullYear()} abhishek`;
 
+let mouseDown = false;
+let erase = "dont";
+const container = document.querySelector('.container');
+
 const gridBuilder = () => {
     let val = parseInt(document.querySelector('select').value, 10);
-    const container = document.querySelector('.container');
     let canvasWidth = parseInt(window.getComputedStyle(container).width);
+
     container.innerHTML = '';
-    console.log(val + ' ' + canvasWidth);
-    console.log( canvasWidth / val);
+
     for(let i=0; i<(val*val); ++i){
         let div = document.createElement('div');
+
         div.style.width = `${canvasWidth/val}px`;
         div.style.aspectRatio = 1;
+        div.style.border = '1px black solid';
         div.classList.add('bg-white');
-        div.addEventListener('click', () => {
-            div.style.backgroundColor = 'black';
-        })
+
+        div.addEventListener('mousedown', ()=> mouseDown = true);
+
+        div.addEventListener('mouseup', ()=> mouseDown=false);
+
+        div.addEventListener('click', (event) => {
+            div.style.backgroundColor=erase=="erase"?'white':'black';
+        });
+
+        div.addEventListener('mousemove', (event) => {
+            if(mouseDown){
+                div.style.backgroundColor=erase=="erase"?'white':'black';
+            }
+        });
+
+        container.addEventListener('contextmenu', (event) => event.preventDefault());
         container.appendChild(div);
     }
 }
+container.addEventListener('mouseleave', () => mouseDown=false);
+document.addEventListener('dragstart', (event)=> event.preventDefault());
 gridBuilder();
 document.querySelector('select').addEventListener('change',gridBuilder);
+
+const eraser = () => {
+    erase = erase=="erase"?"dont":"erase";
+    document.querySelector('#eraser').classList.toggle('enabled');
+}
